45ac7c94536fb689230eb8ab6cbef1b58c962ba6
Krystian <klisowski923@gmail.comgit config --globa user.name Krystian9292git config --global user.name Krystian9292git config --globa user.email klisowski923@gmail.com>
2023-10-27 15:04:16 +0200

    Warsztat zadania

--- Warsztaty Baza Danych/create_db.py
+++ Warsztaty Baza Danych/create_db.py
@@ -1,3 +1,6 @@
+from psycopg2 import connect, OperationalError
+from psycopg2.errors import DuplicateTable, DuplicateDatabase
+
 Create_db = "CREATE DATABASE workshop;"
 
 Create_tb = """CREATE TABLE users(
@@ -9,7 +12,55 @@ Create_tb = """CREATE TABLE users(
 Create_tb1 = """CREATE TABLE messages(
     id serial PRIMARY KEY,
     from_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    to_id INTEGER REFERENCES user(id) ON DELETE CASCADE
-    creation_date timestamp,
+    to_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    creation_date TIMESTAMP,
     text varchar(255)
 )"""
+
+DB_USER = "postgres"
+DB_PASSWORD = "coderslab"
+DB_HOST = "127.0.0.1"
+
+try:
+    cnx = connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+
+    try:
+        cursor.execute(Create_db)
+        print("Database created")
+
+    except DuplicateDatabase as e:
+        print("Database exists ", e)
+
+    cnx.close()
+
+except OperationalError as e:
+    print("Connection Error: ", e)
+
+
+
+try:
+    cnx = connect(database="workshop", user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+
+
+    try:
+        cursor.execute(Create_tb)
+        print("Table users created")
+
+    except DuplicateTable as e:
+        print("Table exists ", e)
+
+
+    try:
+        cursor.execute(Create_tb1)
+        print("Table messages created")
+
+    except DuplicateTable as e:
+        print("Table exists ", e)
+    cnx.close()
+
+except OperationalError as e:
+    print("Connection Error: ", e)
